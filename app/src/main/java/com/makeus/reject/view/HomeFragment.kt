2fd4eccc77d7a43156bf43f70dba716651831e76
DongChyeon <kimdonghyun90766@gmail.com>
2023-01-07 18:40:06 +0900

    아이콘, 툴바 추가

--- app/src/main/java/com/makeus/reject/view/HomeFragment.kt
+++ app/src/main/java/com/makeus/reject/view/HomeFragment.kt
@@ -2,31 +2,16 @@ package com.makeus.reject.view
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.lifecycleScope
 import com.makeus.reject.R
 import com.makeus.reject.base.BaseFragment
 import com.makeus.reject.databinding.FragmentHomeBinding
-import com.makeus.reject.network.model.request.SignupReq
 import com.makeus.reject.network.repository.UserRepository
-import kotlinx.coroutines.launch
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private lateinit var userRepository: UserRepository
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.button.setOnClickListener {
-            userRepository = UserRepository()
-            lifecycleScope.launch {
-                userRepository.signup(SignupReq("abcd", "abcd", "abcd", "abcd", "abcd")).onSuccess {
-
-                }.onFailure {
-
-                }
-            }
-        }
-
     }
 
 }
--- app/src/main/java/com/makeus/reject/view/MainActivity.kt
+++ app/src/main/java/com/makeus/reject/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.makeus.reject.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -20,6 +22,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar!!.setIcon(R.drawable.ic_app_logo)
+
         val host: NavHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment? ?: return
         val navView: BottomNavigationView = binding.navView
@@ -27,5 +33,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         navView.setupWithNavController(navController)
 
         this.onBackPressedDispatcher.addCallback(this, backPressedCallback)
+    }
+
+    // 메뉴 리소스 XML의 내용을 앱바(App Bar)에 반영
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    //item 버튼 클릭 했을 때
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.search -> {
+                // 검색
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
