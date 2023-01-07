c321cb745df3a1e73722e018e156568f8766af66
godgo <godgod153@gmail.com>
2023-01-07 18:00:39 +0900

    splash BaseActivity로 변경

--- app/src/main/java/com/makeus/reject/view/SplashActivity.kt
+++ app/src/main/java/com/makeus/reject/view/SplashActivity.kt
@@ -1,16 +1,18 @@
 package com.makeus.reject.view
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.appcompat.app.AppCompatActivity
 import com.makeus.reject.R
+import com.makeus.reject.base.BaseActivity
+import com.makeus.reject.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
