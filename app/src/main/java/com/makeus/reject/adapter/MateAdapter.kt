76d0f9116e515a59f494fa6f9ad2d025fc1f4a7c
DongChyeon <kimdonghyun90766@gmail.com>
2023-01-07 21:07:19 +0900

    바텀 네비게이션 아이템 수정, 홈 프래그먼트 추가

---
+++ app/src/main/java/com/makeus/reject/adapter/MateAdapter.kt
@@ -0,0 +1,64 @@
+package com.makeus.reject.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.makeus.reject.R
+
+class MateAdapter : ListAdapter<Int, MateAdapter.ViewHolder>(MateComparator()) {
+    private lateinit var listener: OnItemClickListener
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+        holder.itemView.setOnClickListener {
+            listener.onItemClick(it, position)
+        }
+    }
+
+    fun getMate(position: Int): Int {
+        return getItem(position)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView = itemView.findViewById<ImageView>(R.id.imageView)
+
+        companion object {
+            fun create(parent: ViewGroup): ViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.mate_item, parent, false)
+                return ViewHolder(view)
+            }
+        }
+
+        fun bind(image: Int) {
+            imageView.setImageResource(image)
+        }
+    }
+
+    class MateComparator : DiffUtil.ItemCallback<Int>() {
+        override fun areItemsTheSame(oldItem: Int, newItem: Int): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Int, newItem: Int): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/makeus/reject/view/HomeFragment.kt
+++ app/src/main/java/com/makeus/reject/view/HomeFragment.kt
@@ -2,16 +2,51 @@ package com.makeus.reject.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.makeus.reject.R
+import com.makeus.reject.adapter.BannerAdapter
+import com.makeus.reject.adapter.CompetitionAdapter
+import com.makeus.reject.adapter.MateAdapter
 import com.makeus.reject.base.BaseFragment
 import com.makeus.reject.databinding.FragmentHomeBinding
-import com.makeus.reject.network.repository.UserRepository
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    private lateinit var userRepository: UserRepository
+    private lateinit var bannerAdapter: BannerAdapter
+    private lateinit var competitionAdapter: CompetitionAdapter
+    private lateinit var mateAdapter: MateAdapter
+
+    private val bannerList =
+        listOf(R.drawable.bg_284ee3_radius_6dp, R.drawable.bg_ffffff_line_c6c6c6_radius_6dp)
+    private val competitionList = listOf(
+        R.drawable.competition_1,
+        R.drawable.competition_2,
+        R.drawable.competition_3,
+        R.drawable.competition_4,
+        R.drawable.competition_5
+    )
+    private val mateList =
+        listOf(R.drawable.bg_284ee3_radius_6dp, R.drawable.bg_ffffff_line_c6c6c6_radius_6dp)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bannerAdapter = BannerAdapter()
+
+        binding.bannerRecyclerView.adapter = bannerAdapter
+        binding.bannerRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        bannerAdapter.submitList(bannerList)
+
+        competitionAdapter = CompetitionAdapter()
+        binding.competitionRecyclerView.adapter = competitionAdapter
+        binding.competitionRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        competitionAdapter.submitList(competitionList)
+
+        mateAdapter = MateAdapter()
+        binding.mateRecyclerView.adapter = mateAdapter
+        binding.mateRecyclerView.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        mateAdapter.submitList(mateList)
     }
 
 }
